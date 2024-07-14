9dca5ce7d2a5fbacca6c41ec0236ab288227d45d
Shekhar3742 <imderek3742@gmail.com>
2024-07-14 15:45:14 +0530

    Day1

--- Day1.js
+++ Day1.js
@@ -1,1 +1,45 @@
-let hello = "hello";
+//Task 1
+var number = 12;
+console.log(number);
+
+//Task 2
+let hi = "hello";
+console.log(hi);
+
+//Task 3
+const balu1 = true;
+const balu2 = false;
+
+console.log(balu1);
+console.log(balu2);
+
+//Task 4
+let num = 14;
+let strnn = "hello boss";
+let bolllll1 = true;
+let bolllll2 = false;
+let person1 = {
+    name: "rahul",
+    age: 12,
+    color: "blue"
+}
+const cars = ["AUDI", "BMW"];
+
+console.log(typeof num);
+console.log(typeof strnn);
+console.log(typeof bolllll1);
+console.log(typeof bolllll2);
+console.log(typeof person1);
+console.log(typeof cars);
+
+//Task 5
+let model = "BMW";
+console.log(model);
+model = "AUDI";
+console.log(model);
+
+// //Task 6
+// const models = "BMW";
+// console.log(model);
+// models = "AUDI";
+// console.log(model);
